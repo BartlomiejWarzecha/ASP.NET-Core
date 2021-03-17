c522527f140fe8ca62b7da6cf91acac459612042
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2021-03-17 10:01:10 +0100

    Adding 2 methods get and post
    -Checking different sytuations how to use method post
    -different methods with http code(i.e 404 - not found, 403 - not
    permitted access)

--- Restaurant API/Restaurant API/Controllers/WeatherForecastController.cs
+++ Restaurant API/Restaurant API/Controllers/WeatherForecastController.cs
@@ -30,5 +30,25 @@ namespace Restaurant_API.Controllers
 
         }
 
+        [HttpGet("currentDay/{max}")]
+        public IEnumerable<WeatherForecast> GET2([FromQuery]int take, [FromRoute]int max)
+        {
+
+            var result = _service.Get();
+            return result;
+
+        }
+
+        [HttpPost]
+        public ActionResult<string> Hello([FromBody] string name)
+        {
+            // HttpContext.Response.StatusCode = 401; 
+
+            // return StatusCode(401, $"Hello {name}");
+
+            return NotFound($"Hello {name}");
+
+        }
+
     }
 }
