e5694125501c7f4ccb43133a8247ee3dfce823c3
Bartłomiej Warzecha <bartlomiejwarzecha2@gmail.com>
2021-03-29 09:03:24 +0200

    Creating new ASP. NET project Contoso Crafts

---
+++ Restaurant API/Restaurant API/Entities/Dish.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Restaurant_API.Entities
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public int RestaurantId { get; set; }
+        public virtual Restaurant  Restaurant { get; set; }
+    }
+}
